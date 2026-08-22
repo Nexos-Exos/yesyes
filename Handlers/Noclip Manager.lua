c991a4e23a9e39d3e929c4f3b0513aa062291c61
Nexos-Exos <exolesx@gmail.com>
2026-08-22 15:33:59 -0300

    Update Noclip Manager.lua

--- Handlers/Noclip Manager.lua
+++ Handlers/Noclip Manager.lua
@@ -28,7 +28,7 @@ function Noclip:Enable()
   print("Noclip Enabled")
   Noclip.On = true
   
-  Noclip.Connection = Heartbeat:Connect(function()
+  Connection = Heartbeat:Connect(function()
     if Noclip.On then
       for i, v in next, QueryDescendants(Character, "BasePart") do
         v.CanCollide = false
@@ -40,7 +40,7 @@ end
 function Noclip:Disable()
   print("Noclip Disabled")
   Noclip.On = false
-  Noclip.Connection:Disconnect()
+  Connection:Disconnect()
   
   for i, v in next, QueryDescendants(Character, "BasePart") do
     if not table.find(BodyParts, v.Name) then v.CanCollide = true end
